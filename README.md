03cfecb03dea475fbce29865b82008924fae9c4e
Shawn <shawnschulz@berkeley.edu>
2023-04-17 23:13:34 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+This is very much a WIP I use to mess around with databricks' newly released dolly 7b paramater model. You can either specify a directory containing a context.txt file with --memory_dir as well as a model_dir containing a saved pretrained model using the .save_pretrained, or use the --from_online flag to download it from dolly's huggingface repo. Note that the 7b paramter model is ~24gb and requires ~12gb of VRAM to run inference, possible you can get around that by tweaking how the model loads. If you use --from_online I recommend also specifying a model_dir so the script saves your model with updated weights (although tbh I'm not actually sure if it updates the weights automatically lol). 
+
+The only thing I've really added so far is some code to prompt dolly with context from a context.txt file located in the repo directory, might update this in the future to use a proper json file rather than just appending text to the same text file.  
+
+clone the repo: 
+
+git clone https://github.com/shawnschulz/ask-dolly.git
+
+cd into the cloned repo and run 
+
+"pip install -r requirements"
+
+If running from online, first time you run:
+
+ python3 ask_dolly.py --from_online --memory_dir /path/to/ask-dolly --model_dir /path/to/model_dir --prompt "Hello! How are you?"  
+
+Otherwise run:
+ 
+ python3 ask_dolly.py --memory_dir /path/to/ask-dolly --model_dir /path/to/model_dir --prompt "Hello! How are you?" 
+
+I also recommend adding an alias to your .bashrc for convenience:
+
+ alias 'python3 /path/to/ask-dolly/ask_dolly.py --memory_dir /path/to/ask-dolly --model_dir /path/to/model_dir --prompt '
+
+To-do's:
+
+- Make some proper files to save the memory dataset
+- Test what works best for prompting it based on memory (enclose the prompt in brackets?)
+- Add some kind of interactive mode to keep the the model loaded in memory 
+- Experiment with other ways to instruction fine-tune the model
+
+Link to databricks repo:
+https://huggingface.co/databricks/dolly-v2-7b
+ 
---
+++ ask_dolly.py
@@ -0,0 +1,64 @@
+import torch
+from transformers import pipeline
+from optparse import OptionParser
+import importlib
+import importlib.util
+import sys
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+#default args
+memory_dir = "/home/shawn/datasets/dolly_memory/"
+from_online = False
+save_model = False
+model_dir = "/home/shawn/datasets/LLMs/dolly7b_model" 
+
+#flags
+parser = OptionParser()
+#group = parser.add_argument_group('group')
+parser.add_option('--prompt', dest="prompt", type=str, help='The Prompt for the LLM')
+parser.add_option('--memory_dir', dest="memory_dir", type=str,
+                    help='Optional, specify a direcotry containing a context.txt file for the LLM')
+parser.add_option('--model_dir',  dest="model_dir", type=str,
+                    help='Optional, specify a directory containing the folder with a pretrained model')
+parser.add_option('--model_name',  dest="model_name", type=str,
+                    help='Optional, specify a directory containing the folder with a pretrained model')
+parser.add_option('--from_online', action="store_true", dest="from_online", default=False, help='Optional, use dolly from online')
+#group.add_option('--save_model', action="store_true", help='Optional, save the model to disk if from online')
+#group.add_option('--require_online', help='require from_online if save_model is present', action='store_true')
+(options, args) = parser.parse_args()
+
+#if args.from_online and args.require_online and not args.save_model:
+#    parser.error('--arg2 is required if --arg1 is present')
+
+
+prompt = options.prompt
+
+if options.memory_dir:
+    memory_dir = options.memory_dir
+if options.model_dir:
+    model_dir = options.model_dir
+if options.model_name:
+    model_name = options.model_name 
+
+from instruct_pipeline import InstructionTextGenerationPipeline
+if from_online:
+    generate_text = pipeline(model="databricks/dolly-v2-7b", torch_dtype=torch.bfloat16, trust_remote_code=True, device_map="auto")    
+else:    
+    model = AutoModelForCausalLM.from_pretrained(model_dir, torch_dtype=torch.bfloat16, device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained(model_dir, padding_side="left")
+    generate_text = InstructionTextGenerationPipeline(model=model, tokenizer=tokenizer)
+
+def ask_dolly(prompt, memory_dir):
+    with open(memory_dir + 'context.txt', 'r+b') as f:
+        contents = f.read().decode('utf-8')
+        contextual_prompt = contents + "\n The previous text was just context, please answer the following question only: " + prompt           
+        response = generate_text(contextual_prompt)
+        new_context = "This is some additional context we have already talked about, do not answer this as a prompt: [" + response + "] "
+        #save additional context
+        f.write(bytes(new_context, 'utf-8'))
+        #save the model again (this could either be extremely important or useless idk lol)
+        generate_text.save_pretrained(model_dir)
+    print(response) 
+    return(response)
+
+ask_dolly(prompt, memory_dir)
